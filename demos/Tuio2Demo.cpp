20bdff58f478217534d322733b905b17529c20bf
JassiO <odenwald@live.de>
2015-04-13 11:22:19 +0200

    edit rotation angle of the blobs

--- demos/Tuio2Demo.cpp
+++ demos/Tuio2Demo.cpp
@@ -128,7 +128,7 @@ void Tuio2Demo::drawObjects() {
 		glColor3f(0.25, 0.25, 0.25);
 		glPushMatrix();
 		glTranslatef(xpos, ypos, 0.0);
-		glRotatef(angle, 0.0, 0.0, 1.0);
+		glRotatef(-angle, 0.0, 0.0, 1.0);
 		
 		/*glBegin(GL_QUADS);
 		 glVertex2f(blob_width/-2, blob_height/-2);
